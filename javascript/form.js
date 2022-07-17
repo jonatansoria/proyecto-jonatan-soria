21aea1e732660831741c8f1da36f09c0006d6130
jonatan soria <jonatansoriatw@gmail.com>
2022-07-17 19:11:17 -0300

    pre entrega final

--- javascript/form.js
+++ javascript/form.js
@@ -1,58 +1,43 @@
+//---------guardar informacion del usuario----------
 
-// //--------------formulario------------
+let dateUser =document.querySelector("#btn-confirmar").addEventListener("click",registerUser);
 
-// let formContainer = document.createElement("div")
-// let verFormulario = document.createAttribute("container-form")
-// let formulario = document.querySelector("formulario")
+function registerUser(){
 
-// const datosCompra = document.querySelector(".formulario")
-//    datosCompra.innerHTML =`
-//    <form class="row g-3 needs-validation" novalidate>
-//   <div class="col-md-4">
-//     <div class="form-outline">
-//       <input type="text" class="form-control" id="validationCustom01" value="ingrese su nombre" required />
-//       <label for="validationCustom01" class="form-label">Nombre</label>
-//       <div class="valid-feedback">hola</div>
-//     </div>
-//   </div>
-//   <div class="col-md-4">
-//     <div class="form-outline">
-//       <input type="text" class="form-control" id="validationCustom02" value="Otto" required />
-//       <label for="validationCustom02" class="form-label">Last name</label>
-//       <div class="valid-feedback">Looks good!</div>
-//     </div>
-//   </div>
-//   <div class="col-md-4">
-//     <div class="input-group form-outline">
-//       <span class="input-group-text" id="inputGroupPrepend">@</span>
-//       <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
-//       <label for="validationCustomUsername" class="form-label">Username</label>
-//       <div class="invalid-feedback">Please choose a username.</div>
-//     </div>
-//   </div>
-//   <div class="col-md-6">
-//     <div class="form-outline">
-//       <input type="text" class="form-control" id="validationCustom03" required />
-//       <label for="validationCustom03" class="form-label">City</label>
-//       <div class="invalid-feedback">Please provide a valid city.</div>
-//     </div>
-//   </div>
-//   <div class="col-md-6">
-//     <div class="form-outline">
-//       <input type="text" class="form-control" id="validationCustom05" required />
-//       <label for="validationCustom05" class="form-label">Zip</label>
-//       <div class="invalid-feedback">Please provide a valid zip.</div>
-//     </div>
-//   </div>
-//   <div class="col-12">
-//     <div class="form-check">
-//       <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-//       <label class="form-check-label" for="invalidCheck">Agree to terms and conditions</label>
-//       <div class="invalid-feedback">You must agree before submitting.</div>
-//     </div>
-//   </div>
-//   <div class="col-12">
-//     <button class="btn btn-primary" type="submit">Submit form</button>
-//   </div>
-// </form>
-//    `
+  let user =[],
+     mail= "",
+     nameUser="",
+     adress = "",
+     data="",
+     city="",
+     chekMail= ""
+
+ mail= document.querySelector("#inputEmail").value;
+ nameUser= document.querySelector("#inputname").value;
+ adress= document.querySelector("#inputAddress").value;
+ data= document.querySelector("#inputAddress2").value;
+ city= document.querySelector("#inputcity").value;
+ chekMail= document.querySelector("#gridCheck").value;
+ 
+ user.push(mail,nameUser,adress,data,city,chekMail);
+
+ localStorage.setItem('user', JSON.stringify(user))
+
+ }
+
+
+//--------------alert-compra confirmada---------
+
+const buyProduct =(e) =>{
+     
+    Swal.fire({
+        title:'Muchas gracias por su compra',
+        text:'los datos de su compra fue enviado a su correo',
+        icon:'success',
+        showConfirmButton: true,
+        footer:"Muchas gracias por elegir Turismo Madryn"
+       })
+      e.preventDefault()
+    }
+let buyConfirmation = document.querySelector(".finalizar-compra");
+buyConfirmation.onclick = buyProduct;
--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,4 @@
 //--------------------------Productos en html---------------------------------
-
 const fetchLocal = async () => {
     try{
      const response= await fetch ("./actividades.json")
@@ -24,7 +23,7 @@ const fetchLocal = async () => {
         let amount = document.createElement("h5")
         amount.innerText = (rides.amount)
         let price = document.createElement("h4")
-        price.innerText =("$",rides.price)
+        price.innerText =(rides.price)
         price.setAttribute("class", "precio")
         let idProd = document.createElement("a")
         idProd.innerText = (rides.id)
@@ -62,8 +61,6 @@ const fetchLocal = async () => {
 
 fetchLocal();
 
-
-
 let title = document.getElementById("titulo")
 let seeActivities = document.getElementById("verActividades")
 const div = document.querySelector('.div')
@@ -75,88 +72,11 @@ if (localStorage.getItem('cart')) {
     watchcart(true)
 }
 
-
-
-
-//-----------cart----------------
-
-let seeCart = document.querySelector(".container-carrito")
-let showcart = document.querySelector(".carrito")
-let deleteItem = document.querySelector(".eliminar")
-
-//------productos del cart------------
-
-function removeItem(id) {
-    document.getElementById(id).remove()
-
-    const findItem = cart.find(element => element.id == id)
-    findItem.amount = 1
-     
-    cart = cart.filter(element => element.id != id)
-    const total = cart.reduce((acc, item) => acc + (item.price * item.amount), 0)
-    document.getElementById("totalcart").innerText = ("Precio Total: $" + total)
-    if (!cart.length) {
-        document.getElementById("totalcart").remove()
-    }
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
-
-function watchcart(first) {
-    if (!first) {
-
-        alertCart.remove()
-    }
-    cart.forEach((element) => {
-        const divCart = document.createElement('tr')
-        divCart.id = `${element.id}`
-        divCart.innerHTML += ` 
-        <td class="col-md-3"><img class="img-cart" src="${element.img}"></td>
-        <td class="col-md-2"><h3>${element.name}</h3></td>
-        <td class="col-md-2"><h5>cantidad: ${element.amount}<h5></td>
-        <td class="col-md-2"><h3>${element.price * element.amount}</h3></td>
-        <td class="col-md-2"><button class="eliminar" onclick="removeItem('${divCart.id}')" data-id=${element.id}>Quitar</button></td><hr>`
-        div.setAttribute("class", "listado")
-        div.appendChild(divCart) 
-      
-})
-
-    //-----------------total-------------------
-    const total = cart.reduce((acc, item) => acc + (item.price * item.amount), 0)
-    let totalbuy = document.createElement("h3")
-    totalbuy.id = "totalcart"
-    totalbuy.setAttribute("class", "total")
-    if (total) {
-        totalbuy.innerText = ("Precio Total: $" + total)
-    }
-    div.append(totalbuy)
-
-    localStorage.setItem('cart', JSON.stringify(cart))
-
-}
-
-//---------aviso de cart vacio--------------
-let alertCart = document.createElement("h3")
-alertCart.setAttribute("class", "vacio")
-
-if (!cart.length) {
-    alertCart.innerText = ("El carrito está vacío")
-    div.append(alertCart)
-} else { alertCart.remove() }
-
-//-------------fconfirmar compra---------------
-//    let cerrarCompra = document.querySelector(".eliminar")
-//     cerrarCompra.addEventListener ( 
-//         div.append(datosCompra)
-//     )
-
-
-
-//--------------ventana modal(cart)-------------------------
+//-----------------------ventana modal(carrito)--------------------------------------
 
 const openCart = document.querySelector('.cart-img');
 const cartCont = document.querySelector('.container-carrito');
 const closeCart = document.querySelector('.cerrar-carrito');
-
 
 openCart.addEventListener('click', (event) => {
     event.preventDefault()
@@ -168,11 +88,101 @@ closeCart.addEventListener('click', function(event) {
     cartCont.classList.remove('ventana-carrito')
 });
 
+//-------------------------carrito----------------------------------
+
+let seeCart = document.querySelector(".container-carrito")
+let showcart = document.querySelector(".carrito")
+let deleteItem = document.querySelector(".eliminar")
+
+//------productos del carrito------------
+function watchcart(first) {
+    if (!first) {
+        alertCart.remove() 
+       
+    }
+    cart.forEach((element) => {
+        const divCart = document.createElement('tr')
+        divCart.id = `${element.id}`
+        divCart.innerHTML += ` 
+        <td class="col-md-3"><img class="img-cart" src="${element.img}"></td>
+        <td class="col-md-2"><h3>${element.name}</h3></td>
+        <td class="col-md-2"><h5>cantidad: ${element.amount}<h5></td>
+        <td class="col-md-2"><h3>${element.price * element.amount}</h3></td>
+        <td class="col-md-2"><button class="eliminar" onclick="removeItem('${divCart.id}')" data-id=${element.id}>Borrar</button></td><hr>`
+        div.setAttribute("class", "listado")
+        div.appendChild(divCart) 
+})
+//-----------------total-------------------
+
+    const total = cart.reduce((acc, item) => acc + (item.price * item.amount), 0)
+    let totalbuy = document.createElement("h2")
+    totalbuy.id = "totalcart"
+    totalbuy.setAttribute("class", "total")
+    if (total) {
+        totalbuy.innerText = ("Precio Total: $" + total)
+    }
+    div.append(totalbuy)
+
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+//-----------eliminar productos del carrito-----------------
+function removeItem(id) {
+    document.getElementById(id).remove()
+ 
+    const findItem = cart.find(element => element.id == id)
+    findItem.amount = 1
+     
+    cart = cart.filter(element => element.id != id)
+    const total = cart.reduce((acc, item) => acc + (item.price * item.amount), 0)
+    document.getElementById("totalcart").innerText = ("Precio Total: $" + total)
+    if (!cart.length) {
+        document.getElementById("totalcart").remove()
+    }
+    localStorage.setItem('cart', JSON.stringify(cart))        
+} 
+
+const deleteProd = (e) =>{
+
+Swal.fire({
+            title:"eliminaste " + element.name + " del carrito",
+            imageUrl: element.img,
+            imageWidth: 200,
+            imageHeight: 100,
+            imageAlt: 'Custom image',
+            footer:"Muchas gracias por elegir Turismo Madryn"
+          }) 
+           e.preventDefault()
+        }
+          
+let removeProd =document.querySelector("eliminar")
+removeProd.onclick = deleteProd
+
+//---------aviso de carrito vacio--------------
+
+let alertCart = document.createElement("h3")
+alertCart.setAttribute("class", "vacio")
+
+if (!cart.length) {
+    alertCart.innerText = ("El carrito está vacío")
+    div.append(alertCart)
+} else { alertCart.remove() }
+
+//-------quitar formulario de carrito vacio----------
+
+let forms = document.getElementById("form")
+forms.setAttribute("class","forms")
+
+if (!cart.length) {
+    forms.innerText = ("")
+    forms.remove()
+} 
 //------------alerta de suscripcion------------
+
 const confirmation =(e) =>{
      
     Swal.fire({
-        title:'Muchas gracias por suscribirte!!',
+        title:'Muchas gracias por suscribirte',
         text:'Pronto te enviaremos todas nuetras novedades',
         icon:'success',
         showConfirmButton: true
@@ -183,11 +193,4 @@ const confirmation =(e) =>{
 let mail = document.querySelector("form-control")
 let subcription = document.querySelector(".appsLand-btn");
 subcription.onclick = confirmation
- 
-    // Swal.fire(
-    //             "se elimino actividad",
-    //             "lo puedes agregar de nuevo si te arrepientes",
-    //             "error",
-                
-    //           )
 
